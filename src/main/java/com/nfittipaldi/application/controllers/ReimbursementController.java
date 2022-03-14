96eea6117d6c5aaed991feb174192ed0b00990b6
Nicholas Fittipaldi <nfittutc@gmail.com>
2022-03-14 19:16:00 -0400

    Made changes to the reimbursement controller class.

--- src/main/java/com/nfittipaldi/application/controllers/ReimbursementController.java
+++ src/main/java/com/nfittipaldi/application/controllers/ReimbursementController.java
@@ -3,13 +3,17 @@ package com.nfittipaldi.application.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nfittipaldi.application.models.Reimbursement;
 import com.nfittipaldi.application.repositories.ReimbursementRepository;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/reimbursementController/")
 public class ReimbursementController {
@@ -17,8 +21,18 @@ public class ReimbursementController {
 	@Autowired
 	private ReimbursementRepository reimbursementRepository;
 	
-	@GetMapping("/reimbursements")
+	@GetMapping("/getReimbursement")
+	public Reimbursement getReimbursement(@RequestBody long id) {
+		return reimbursementRepository.getById(id);
+	}
+	
+	@GetMapping("/getAllReimbursements")
 	public List<Reimbursement> getAllReimbursements() {
 		return reimbursementRepository.findAll();
+	}
+	
+	@PostMapping("/addReimbursement")
+	public Reimbursement addReimbursement(@RequestBody Reimbursement reimbursement) {
+		return reimbursementRepository.save(reimbursement);
 	}
 }
--- src/main/java/com/nfittipaldi/application/controllers/UserController.java
+++ src/main/java/com/nfittipaldi/application/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,8 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@GetMapping("/users")
-	public List<User> getAllUsers() {
-		return userRepository.findAll();
+	@GetMapping("/getUser")
+	public User getUser(@RequestBody String username) {
+		return null;
 	}
 }
